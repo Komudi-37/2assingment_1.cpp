329ad563eb93c2c928ee7f1e568befa86088071e
Komudi-37 <rkomudi@gmail.com>
2025-02-07 21:07:36 +0530

    Create assingment4.cpp

---
+++ assingment4.cpp
@@ -0,0 +1,39 @@
+//4)spiral no. patteren
+#include<iostream>
+using namespace std;
+void printspiral(int n){
+    int arr[n][n];
+    int num=1;
+    int top=0,bottom=n-1,left=0,right=n-1;
+    while(num<=n*n){
+        for(int i=left;i<=right;i++){
+            arr[top][i]=num++;
+        }
+        top++;
+        for(int i=top;i<=bottom;i++){
+            arr[i][right]=num++;
+        }
+        right--;
+        for(int i=right;i>=left;i--){
+            arr[bottom][i]=num++;
+        }
+        bottom--;
+        for(int i=bottom;i>=top;i--){
+            arr[i][left]=num++;
+        }
+        left++;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"enter the size of the matrix";
+    cin>>n;
+    printspiral(n);
+    return 0;
+}
