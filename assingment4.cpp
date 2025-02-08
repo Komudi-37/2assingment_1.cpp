77eb2124cc04830b26f20bd23920f0ae7da4d29d
Komudi-37 <rkomudi@gmail.com>
2025-02-08 20:21:27 +0530

    Update assingment4.cpp

--- assingment4.cpp
+++ assingment4.cpp
@@ -1,39 +1,76 @@
 //4)spiral no. patteren
-#include<iostream>
+#include <iostream>
 using namespace std;
-void printspiral(int n){
-    int arr[n][n];
-    int num=1;
-    int top=0,bottom=n-1,left=0,right=n-1;
-    while(num<=n*n){
-        for(int i=left;i<=right;i++){
-            arr[top][i]=num++;
+
+void printSpiral(int n, int arr[][100]) {
+    int num = 1;
+    int top = 0, bottom = n - 1, left = 0, right = n - 1;
+
+    while (num <= n * n) {
+        for (int i = left; i <= right; i++) {
+            arr[top][i] = num++;
         }
         top++;
-        for(int i=top;i<=bottom;i++){
-            arr[i][right]=num++;
+        for (int i = top; i <= bottom; i++) {
+            arr[i][right] = num++;
         }
         right--;
-        for(int i=right;i>=left;i--){
-            arr[bottom][i]=num++;
+        for (int i = right; i >= left; i--) {
+            arr[bottom][i] = num++;
         }
         bottom--;
-        for(int i=bottom;i>=top;i--){
-            arr[i][left]=num++;
+        for (int i = bottom; i >= top; i--) {
+            arr[i][left] = num++;
         }
         left++;
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-    }
 }
-int main(){
+
+void spiralTraversal(int n, int arr[][100]) {
+    int top = 0, bottom = n - 1, left = 0, right = n - 1;
+
+    cout << "Spiral traversal: ";
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++)
+            cout << arr[top][i] << " ";
+        top++;
+
+        for (int i = top; i <= bottom; i++)
+            cout << arr[i][right] << " ";
+        right--;
+
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--)
+                cout << arr[bottom][i] << " ";
+            bottom--;
+        }
+
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--)
+                cout << arr[i][left] << " ";
+            left++;
+        }
+    }
+    cout << endl;
+}
+
+int main() {
     int n;
-    cout<<"enter the size of the matrix";
-    cin>>n;
-    printspiral(n);
+    cout << "Enter the size of the matrix: ";
+    cin >> n;
+
+    int arr[100][100]; 
+    printSpiral(n, arr);
+
+    cout << "Generated Spiral Matrix: \n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    spiralTraversal(n, arr);
+
     return 0;
 }
